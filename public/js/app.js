a097aca678ad57d48530f399961fac0d8d3ce32d
LC <lcyoong@YOONGs-MacBook-Pro.local>
2022-02-19 22:09:55 +0100

    Welcome page

--- public/js/app.js
+++ public/js/app.js
@@ -19297,8 +19297,15 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
+var _hoisted_1 = {
+  "class": "px-4 sm:px-6 lg:px-8 h-screen flex flex-col justify-center items-center"
+};
+
+var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<h1 class=\"text-center mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl\">Welcome to Habit Tracker</h1><p class=\"text-center max-w-xl mt-5 text-xl text-gray-500\"> Realize your biggest dreams with small <span class=\"underline underline-offset-4 italic\">daily</span> steps. </p><a class=\"my-6 inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-gradient-to-r from-indigo-500 to-lime-500 hover:from-lime-500 hover:to-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500\" href=\"/my-tracker\">Take the first step</a><div class=\"mt-36\">Built with love</div>", 4);
+
+var _hoisted_6 = [_hoisted_2];
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("h1", null, "Hello");
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, _hoisted_6);
 }
 
 /***/ }),
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Hello</h1>
+  <div class="px-4 sm:px-6 lg:px-8 h-screen flex flex-col justify-center items-center">
+    <h1
+      class="text-center mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl"
+    >Welcome to Habit Tracker</h1>
+    <p class="text-center max-w-xl mt-5 text-xl text-gray-500">
+      Realize your biggest dreams with small
+      <span class="underline underline-offset-4 italic">daily</span> steps.
+    </p>
+    <a
+      class="my-6 inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-gradient-to-r from-indigo-500 to-lime-500 hover:from-lime-500 hover:to-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      href="/my-tracker"
+    >Take the first step</a>
+
+    <div class="mt-36">Built with love</div>
+  </div>
 </template>
 
 <script>
