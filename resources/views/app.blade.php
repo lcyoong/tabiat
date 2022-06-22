9279eccfcef5ec215307c6cafb22b47c2a0f79bb
LC Yoong <lcyoong@LCs-MacBook-Pro.local>
2022-06-22 11:52:29 +0800

    Making the app.js and app.css relative path

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('js/app.js') }}" defer></script>
     @routes
     @inertiaHead
   </head>
