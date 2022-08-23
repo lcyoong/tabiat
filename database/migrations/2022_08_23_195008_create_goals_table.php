e1a9f6253c00ed08480514ef83a161a0bf5e6366
LC Yoong <lcyoong@lcs-macbook-pro.home>
2022-08-23 21:58:54 +0200

    Goals

---
+++ database/migrations/2022_08_23_195008_create_goals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->increments('gol_id');
+            $table->string('gol_name', 255);
+            $table->string('gol_photo', 255);
+            $table->text('gol_description')->nullable();
+            $table->date('gol_date_from')->nullable();
+            $table->date('gol_date_to')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+}
