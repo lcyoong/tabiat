a445e0d02668e34e6d5fb5d20e786220d579aa6d
LC Yoong <lcyoong@LCs-MacBook-Pro.local>
2022-08-25 22:23:59 +0200

    Create alias for resource/js path and ziggy

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,9 @@ mix.js('resources/js/app.js', 'public/js')
     .vue(3)
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]).version();
+    ])
+    .alias({
+        '@': 'resources/js',
+        ziggy: "vendor/tightenco/ziggy/dist/vue",
+    })    
+    .version();
